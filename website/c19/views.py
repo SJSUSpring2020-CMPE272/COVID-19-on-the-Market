cda89be38c53e5466146c01168d19c85903663ea
Michaela Molina <mijmolina17@gmail.com>
2020-04-30 16:26:55 -0700

    website is capable of displaying graph

--- website/c19/views.py
+++ website/c19/views.py
@@ -6,23 +6,69 @@ import json
 import urllib3
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os 
+
 
 from .forms import FieldsForm
 
+def run_script():
+    initialValue = np.array(["2019-01-02", 2.66, 1.7, 0, 0, 0, 220000, 2.329, 3.013])
+    singleRow = pd.DataFrame([initialValue],
+                      columns=["date", 'DGS10[\%]', "T10YIE[\%]", "Confirmed Cases", "Recovered Cases",
+                                "Deaths", "Unemployment Claims", "Weekly Gas Avg", "Weekly Diesal Avg"])
+    #addedValue = singleRow
+    #for n in range(0,500):
+    #    singleRow['Unemployment Claims'] = int(singleRow['Unemployment Claims'].values[0]) + 100
+    #    addedValue = pd.concat([addedValue, singleRow])
+    #values = addedValue.values.tolist() 
+    input_array = [[2.66, 1.7, 1000, 200, 50, 220100.0, 2.329, 3.013],
+		     						 [0.73,0.73,4632,17,85,3307000,2.343,2.733],
+		    							  [0.76,1,959,8,28,282000,2.343,2.733],
+		   									   [0.92,0.75,19100,147,244,3307000,2.217,2.659],
+		      										[0.72,0.94,121478,1072,2026,6648000,2.103,2.586]]
+
+
+    payload = construct_payload(input_array)
+    predictions = getPrediction(payload)
+    print("predictions = " + str(predictions))
+    
+    #predictions = {'predictions': [{'fields': ['prediction'], 'values': [[2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2536.1139892578126], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2536.1139892578126], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2536.1139892578126], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877],]}]}
+
+    dataPoints = predictions["predictions"][0]["values"]
+    y = np.linspace(1, len(dataPoints), len(dataPoints))
+    plt.plot(y, dataPoints)
+
+    
+    #my_path = os.path.abspath(__file__)
+    #if os.path.isfile('c19/static/images/test_graph.png'):
+    #    os.remove('c19/static/images/test_graph.png')
+    #plt.savefig('c19/static/images/test_graph.png')
+   
+
+
 def generate_iam_token():
         headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/json'}
-        data = {'grant_type':'urn:ibm:params:oauth:grant-type:apikey', 'apikey':'zchGXyAjaMtNldcCjII5EuLdc_xFxkJhagKIc32FHxV_'}
+        # apikey = 'zchGXyAjaMtNldcCjII5EuLdc_xFxkJhagKIc32FHxV_' # Raymond's
+        #apikey = '7SddOmPfjxJu27xUfPzgilmM3P8xTQ3tQmv5C32oCqYn' # Yathu's ?? doesn't work
+        apikey = 'SQu0y8zp7XZNk216RxUY-wZuJmuU9A6oV4vSBN0Yjq1_'  # this works
+        data = {'grant_type':'urn:ibm:params:oauth:grant-type:apikey', 'apikey':apikey}
         server = "https://iam.bluemix.net/oidc/token"
         IBM_cloud_IAM_uid = "bx"
         IBM_cloud_IAM_pw = "bx"
         auth = (IBM_cloud_IAM_uid, IBM_cloud_IAM_pw)
-        response = requests.post(server, headers=headers, data=data, auth=auth)
+        response = requests.post(server, headers=headers, data=data, auth=auth) 
         response_json = json.loads(response.text)
-        access_token = response_json['access_token']
+        print("response_json = " + str(response_json))
+        access_token = response_json['access_token'] 
+        print("access token = " + access_token)
         return access_token
 
 def get_ml_instance_id():
-    return "54369f40-6048-478f-ba00-5c1653af7aa6"
+    # return "54369f40-6048-478f-ba00-5c1653af7aa6" # Raymond's
+    return "e3ac4a09-8322-48a8-b5ab-95ecc4d0924e" # Yathu's
 
 def construct_payload(inputArray):
     payload = {"input_data": 
@@ -43,9 +89,9 @@ def construct_payload(inputArray):
     return payload
 
 
-
 def get_server_url():
     url = 'https://us-south.ml.cloud.ibm.com/v4/deployments/3861dfa6-8e65-48f2-b7a3-9bef448a3ea2/predictions'
+    url = 'https://us-south.ml.cloud.ibm.com/v4/deployments/8adaddce-b306-49c1-8853-c6e1ce8cb6fb/predictions'
     return url
 
 def construct_header(iam_token, ml_instance_id):
@@ -55,17 +101,17 @@ def construct_header(iam_token, ml_instance_id):
     return header
 
 def getPrediction(input_payload):
-    #ml_instance_id  = get_ml_instance_id()
-    #iam_token = generate_iam_token()
+    ml_instance_id  = get_ml_instance_id()
+    iam_token = generate_iam_token()
     #print("iam_token = ", iam_token)
-    #header = construct_header(iam_token, ml_instance_id)
-    #payload = input_payload
-    #url = get_server_url()
+    header = construct_header(iam_token, ml_instance_id)
+    payload = input_payload
+    url = get_server_url()
 
-    #response = requests.post(url, json=payload, headers=header)
-    #context = json.loads(response.text)
+    response = requests.post(url, json=payload, headers=header)
+    context = json.loads(response.text)
     #print("context = ", context)
-    context = {}
+    #context = {}
     return context
 
         
@@ -104,6 +150,8 @@ def receive(request):
         return HttpResponse(template.render(context, request))
 
 def index(request):
+   
+    #run_script()
     form = FieldsForm()
     context = {'form': form}
     template = loader.get_template('c19/index.html')
