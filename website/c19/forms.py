34c36251ee58dba2cd415e7d0cff877196f747d2
Michaela Molina <mijmolina17@gmail.com>
2020-05-04 13:22:53 -0700

    updated style; website can take user input through sliders

--- website/c19/forms.py
+++ website/c19/forms.py
@@ -1,13 +1,99 @@
 from django import forms 
 import datetime
 class FieldsForm(forms.Form):
-        key = forms.DateField(label='Date', initial=datetime.date.today)
-        dgs = forms.DecimalField(label='dgs') 
-        t10 = forms.DecimalField(label='t10') 
-        confirmed_cases = forms.IntegerField(label='confirmed_cases')
-        recovered_cases = forms.IntegerField(label='recovered_cases')
-        deaths = forms.IntegerField(label='deaths')
-        unemployment = forms.IntegerField(label='unemployment')
-        gas_prices = forms.DecimalField(label='gas_prices')
-        diesel_prices = forms.DecimalField(label='diesel_prices')
+
+        '''
+        1. d = dgs
+        2. t = t10
+        3. conf = confirmed_cases
+        4. rec = recovered_cases
+        5. dth = death
+        6. un = enemployment
+        7. gas = gas_prices
+        8. dies = diesel_prices 
+        '''
+
+        # Row 1
+        d1 = forms.CharField(widget=forms.NumberInput(attrs={'id': 'd1_range','class': 'slider-range', 'type':'range', 'value': '2.66', 'step': '.01', 'min': '1.00', 'max': '100.00'}), required=False)
+        t1 = forms.CharField(widget=forms.NumberInput(attrs={'id': 't1_range','type':'range', 'value': '1.7', 'step': '.01', 'min': '1.00', 'max': '100.00'}), required=False)
+        conf1 = forms.CharField(widget=forms.NumberInput(attrs={'id': 'conf1_range','type':'range','value': '1000', 'step': '1', 'min': '1', 'max': '1000000'}), required=False)
+        rec1 = forms.CharField(widget=forms.NumberInput(attrs={'id': 'rec1_range','type':'range', 'value': '200','step': '1', 'min': '1', 'max': '1000000'}), required=False)
+        dth1 = forms.CharField(widget=forms.NumberInput(attrs={'id': 'dth1_range','type':'range', 'value': '50','step': '1', 'min': '1', 'max': '1000000'}), required=False)
+        un1 = forms.CharField(widget=forms.NumberInput(attrs={'id': 'un1_range','type':'range','value': '220100.0', 'step': '.01', 'min': '1', 'max': '1000000.0'}), required=False)
+        gas1 = forms.CharField(widget=forms.NumberInput(attrs={'id': 'gas1_range','type':'range', 'value': '2.329','step': '.001', 'min': '1', 'max': '25.000'}), required=False)
+        dies1 = forms.CharField(widget=forms.NumberInput(attrs={'id': 'dies1_range','type':'range', 'value': '3.013','step': '.001', 'min': '1', 'max': '25.000'}), required=False)
+        
+        #d1    = forms.DecimalField(label='d1') 
+        #t1    = forms.DecimalField(label='t1') 
+        #conf1 = forms.IntegerField(label='conf1')
+        #rec1  = forms.IntegerField(label='rec1') 
+        #dth1  = forms.IntegerField(label='dth1')
+        #un1   = forms.IntegerField(label='un1')
+        #gas1  = forms.DecimalField(label='gas1')
+        #dies1 = forms.DecimalField(label='dies1')
+        #sel1  = forms.BooleanField(label='sel1')
+
+        # Row 2
+        '''d2    = forms.DecimalField(label='d2') 
+        t2    = forms.DecimalField(label='t2') 
+        conf2 = forms.IntegerField(label='conf2')
+        rec2  = forms.IntegerField(label='rec2') 
+        dth2  = forms.IntegerField(label='dth2')
+        un2   = forms.IntegerField(label='un2')
+        gas2  = forms.DecimalField(label='gas2')
+        dies2 = forms.DecimalField(label='dies2')
+        sel2  = forms.BooleanField(label='sel2')
+
+        # Row 3
+        d3    = forms.DecimalField(label='d3') 
+        t3    = forms.DecimalField(label='t3') 
+        conf3 = forms.IntegerField(label='conf3')
+        rec3  = forms.IntegerField(label='rec3') 
+        dth3  = forms.IntegerField(label='dth3')
+        un3   = forms.IntegerField(label='un3')
+        gas3  = forms.DecimalField(label='gas3')
+        dies3 = forms.DecimalField(label='dies3')
+        sel3  = forms.BooleanField(label='sel3')
+
+        # Row 4
+        d4    = forms.DecimalField(label='d4') 
+        t4    = forms.DecimalField(label='t4') 
+        conf4 = forms.IntegerField(label='conf4')
+        rec4  = forms.IntegerField(label='rec4') 
+        dth4  = forms.IntegerField(label='dth4')
+        un4   = forms.IntegerField(label='un4')
+        gas4  = forms.DecimalField(label='gas4')
+        dies4 = forms.DecimalField(label='dies4')
+        sel4  = forms.BooleanField(label='sel4')
+
+        # Row 5
+        d5    = forms.DecimalField(label='d5') 
+        t5    = forms.DecimalField(label='t5') 
+        conf5 = forms.IntegerField(label='conf5')
+        rec5  = forms.IntegerField(label='rec5') 
+        dth5  = forms.IntegerField(label='dth5')
+        un5   = forms.IntegerField(label='un5')
+        gas5  = forms.DecimalField(label='gas5')
+        dies5 = forms.DecimalField(label='dies5')
+        sel5  = forms.BooleanField(label='sel5')
+
+        # Row 6
+        d6    = forms.DecimalField(label='d6') 
+        t6    = forms.DecimalField(label='t6') 
+        conf6 = forms.IntegerField(label='conf6')
+        rec6  = forms.IntegerField(label='rec6') 
+        dth6  = forms.IntegerField(label='dth6')
+        un6   = forms.IntegerField(label='un6')
+        gas6  = forms.DecimalField(label='gas6')
+        dies6 = forms.DecimalField(label='dies6')
+        sel6  = forms.BooleanField(label='sel6')
+
+        #dgs = forms.DecimalField(label='dgs') 
+        #t10 = forms.DecimalField(label='t10') 
+        #confirmed_cases = forms.IntegerField(label='confirmed_cases')
+        #recovered_cases = forms.IntegerField(label='recovered_cases')
+        #deaths = forms.IntegerField(label='deaths')
+        #unemployment = forms.IntegerField(label='unemployment')
+        #gas_prices = forms.DecimalField(label='gas_prices')
+        #diesel_prices = forms.DecimalField(label='diesel_prices')'''
         
--- website/c19/views.py
+++ website/c19/views.py
@@ -5,11 +5,12 @@ import requests
 import json 
 import urllib3
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os 
+from decimal import Decimal
 
 
 from .forms import FieldsForm
@@ -19,11 +20,11 @@ def run_script():
     singleRow = pd.DataFrame([initialValue],
                       columns=["date", 'DGS10[\%]', "T10YIE[\%]", "Confirmed Cases", "Recovered Cases",
                                 "Deaths", "Unemployment Claims", "Weekly Gas Avg", "Weekly Diesal Avg"])
-    #addedValue = singleRow
-    #for n in range(0,500):
-    #    singleRow['Unemployment Claims'] = int(singleRow['Unemployment Claims'].values[0]) + 100
-    #    addedValue = pd.concat([addedValue, singleRow])
-    #values = addedValue.values.tolist() 
+    addedValue = singleRow
+    for n in range(0,500):
+        singleRow['Unemployment Claims'] = int(singleRow['Unemployment Claims'].values[0]) + 100
+        addedValue = pd.concat([addedValue, singleRow])
+    values = addedValue.values.tolist() 
     input_array = [[2.66, 1.7, 1000, 200, 50, 220100.0, 2.329, 3.013],
 		     						 [0.73,0.73,4632,17,85,3307000,2.343,2.733],
 		    							  [0.76,1,959,8,28,282000,2.343,2.733],
@@ -31,22 +32,46 @@ def run_script():
 		      										[0.72,0.94,121478,1072,2026,6648000,2.103,2.586]]
 
 
-    payload = construct_payload(input_array)
-    predictions = getPrediction(payload)
-    print("predictions = " + str(predictions))
+    #payload = construct_payload(input_array)
+    #predictions = getPrediction(payload)
+    #print("predictions = " + str(predictions))
     
-    #predictions = {'predictions': [{'fields': ['prediction'], 'values': [[2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2536.1139892578126], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2536.1139892578126], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2536.1139892578126], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877],]}]}
+    predictions = {'predictions': [{'fields': ['prediction'], 'values': [[2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2536.1139892578126], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2536.1139892578126], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2536.1139892578126], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2521.202978515625], [2551.5869873046877],]}]}
 
     dataPoints = predictions["predictions"][0]["values"]
     y = np.linspace(1, len(dataPoints), len(dataPoints))
-    plt.plot(y, dataPoints)
-
     
+    figure = plt.figure()
+    new_plot = figure.add_subplot(111)
+    new_plot.plot(y, dataPoints, color="#ffffff", linewidth=1, markerfacecolor="#ffffff")
+
+    #plt.plot(y, dataPoints, color="#ffffff", linewidth=3, markerfacecolor="#ffffff")
+    new_plot.spines['bottom'].set_color('#ffffff')
+    new_plot.spines['top'].set_color('#ffffff') 
+    new_plot.spines['right'].set_color('#ffffff')
+    new_plot.spines['left'].set_color('#ffffff')
+
+    for axis in ['bottom','left']:
+        new_plot.spines[axis].set_linewidth(3)
+
+    for axis in ['top','right']:
+        new_plot.spines[axis].set_linewidth(0)
+
+
+    new_plot.tick_params(axis='x', colors='white')
+    new_plot.tick_params(axis='y', colors='white')
+    
+    new_plot.set_xlabel('X-axis')
+    new_plot.set_ylabel('Y-axis')
+
+    new_plot.yaxis.label.set_color('white')
+    new_plot.xaxis.label.set_color('white') 
+
     #my_path = os.path.abspath(__file__)
     #if os.path.isfile('c19/static/images/test_graph.png'):
     #    os.remove('c19/static/images/test_graph.png')
-    #plt.savefig('c19/static/images/test_graph.png')
-   
+    #plt.savefig('c19/static/images/test_graph.png', transparent=True)
+    plt.savefig('c19/static/images/test_graph.png', transparent=True)
 
 
 def generate_iam_token():
@@ -116,7 +141,31 @@ def getPrediction(input_payload):
 
         
 def receive(request):
-        if (request.method == 'POST'):
+    if (request.method == 'POST'):
+            print("In receive: got post request")
+            form = FieldsForm(request.POST)
+            context = {}
+            if form.is_valid():
+                print("The form was valid.")
+                d1 = form.cleaned_data['d1']
+                t1 = form.cleaned_data['t1']
+                conf1 = form.cleaned_data['conf1']
+                rec1 = form.cleaned_data['rec1']
+                dth1 = form.cleaned_data['dth1']
+                un1 = form.cleaned_data['un1']
+                gas1 = form.cleaned_data['gas1']
+                dies1 = form.cleaned_data['dies1']
+                print("d1 = %f" % Decimal(d1))
+                print("t1 = %f" % Decimal(t1))
+                print("conf1 = %d" % Decimal(conf1))
+                print("rec1 = %d" % Decimal(rec1))
+                print("dth1 = %d" % Decimal(dth1))
+                print("un1 = %f" % Decimal(un1))
+                print("gas1 = %f" % Decimal(gas1))
+                print("dies1 = %f" % Decimal(dies1))
+
+            return redirect('/c19/')
+            '''if (request.method == 'POST'):
             print("In Receive: got post request")
             form = FieldsForm(request.POST)
             context = {}
@@ -147,11 +196,12 @@ def receive(request):
                         #"predictions": ML_output['predictions']
                        }
         template = loader.get_template('c19/form_entered.html')
-        return HttpResponse(template.render(context, request))
+        return HttpResponse(template.render(context, request))'''
 
 def index(request):
-   
-    #run_script()
+    
+            
+    run_script()
     form = FieldsForm()
     context = {'form': form}
     template = loader.get_template('c19/index.html')
